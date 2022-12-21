dbfdee398386018703347db136cc49230e37416f
Jimit Patel <78078749+imjimit07@users.noreply.github.com>
2022-12-21 19:46:14 +0400

    added animations

--- layouts/SnippetsLayout.tsx
+++ layouts/SnippetsLayout.tsx
@@ -18,7 +18,7 @@ export default function SnippetsLayout({ posts, title, initialDisplayPosts = [],
 
   return (
     <>
-      <div className="mx-auto max-w-6xl divide-y divide-gray-400">
+      <div className="fade-in mx-auto max-w-6xl divide-y divide-gray-400">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
