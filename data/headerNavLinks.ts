f6654af7a9828e553b6200d6f5436c808c59fc28
Jimit Patel <78078749+imjimit07@users.noreply.github.com>
2022-12-21 04:56:03 +0000

    Added Snippets

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
+  { href: '/snippets', title: 'Snippets' },
   { href: '/courses', title: 'Courses' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
