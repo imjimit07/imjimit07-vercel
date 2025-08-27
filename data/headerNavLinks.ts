a1f180127ce0d646b4e39485c6e4054fb4bb589f
Jimit Patel <78078749+imjimit07@users.noreply.github.com>
2025-08-27 14:44:02 +0530

    removed courses

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/snippets', title: 'Snippets' },
-  { href: '/courses', title: 'Courses' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
   { href: '/contact', title: 'Contact' },
